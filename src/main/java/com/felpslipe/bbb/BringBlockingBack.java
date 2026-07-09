04c6b96c1efb5c3712fb74c84d97645f9273a89c
FelpisLipe <176348835+FelpisLipe@users.noreply.github.com>
2026-07-09 19:50:35 -0300

    Feat: Config reloading and skyblock id support

--- src/main/java/com/felpslipe/bbb/BringBlockingBack.java
+++ src/main/java/com/felpslipe/bbb/BringBlockingBack.java
@@ -2,7 +2,11 @@ package com.felpslipe.bbb;
 
 import com.felpslipe.bbb.config.ConfigManager;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,5 +19,19 @@ public class BringBlockingBack implements ClientModInitializer {
 		client = Minecraft.getInstance();
 		LOGGER.info("[BBB] Bringing blocking back..");
 		ConfigManager.load();
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(ClientCommandManager.literal("bbb")
+					.executes(context -> {
+				context.getSource().sendFeedback(Component.literal("BBB commands:"));
+				context.getSource().sendFeedback(Component.literal("reload - reloads config"));
+				return 1;
+			})
+							.then(ClientCommandManager.literal("reload")
+							.executes(context -> {
+								ConfigManager.load();
+								return 1;
+							}))
+			);
+		});
 	}
 }
--- src/main/java/com/felpslipe/bbb/config/ConfigManager.java
+++ src/main/java/com/felpslipe/bbb/config/ConfigManager.java
@@ -1,13 +1,21 @@
 package com.felpslipe.bbb.config;
 
+import com.felpslipe.bbb.BringBlockingBack;
 import com.google.gson.Gson;
+import com.mojang.blaze3d.platform.InputConstants;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
+import org.lwjgl.glfw.GLFW;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static com.felpslipe.bbb.BringBlockingBack.LOGGER;
+import static com.felpslipe.bbb.BringBlockingBack.client;
 
 public class ConfigManager {
     public static final Gson gson = new Gson();
@@ -30,6 +38,9 @@ public class ConfigManager {
         try(Reader reader = new FileReader(configPath.toFile())) {
             config = gson.fromJson(reader, Config.class);
             LOGGER.info("[BBB] Config loaded successfully");
+            if(client.player != null) {
+                client.player.displayClientMessage(Component.literal("Config Reloaded!"), false);
+            }
         }
         catch(Exception e) {
             LOGGER.error("[BBB] Failed to load config", e);
